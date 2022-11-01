58bfea9218e978932973e005626f65009ce2ffdd
Alper TÜRKMEN <alpertrkmn48@gmail.com>
2022-11-01 12:38:53 +0300

    Pagination and page size

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -5,6 +5,9 @@ import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,20 +22,26 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostDto createPost(PostDto postDto){
-        //convert DTO to entity
+    public PostDto createPost(PostDto postDto) {
+
+        // convert DTO to entity
         Post post = mapToEntity(postDto);
         Post newPost = postRepository.save(post);
 
-        //convert entity to DTO
+        // convert entity to DTO
         PostDto postResponse = mapToDTO(newPost);
         return postResponse;
     }
 
     @Override
-    public List<PostDto> getAllPosts(){
-        List<Post> posts = postRepository.findAll();
-        return posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+    public List<PostDto> getAllPosts(int pageNo, int pageSize){
+        //create pageable instance
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        //get content from page object
+        List<Post> listOfPosts = posts.getContent();
+        return listOfPosts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
     }
 
     @Override
@@ -45,9 +54,6 @@ public class PostServiceImpl implements PostService {
     public PostDto updatePost(PostDto postDto, long id) {
         //get post by id from the db
         Post post  = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
-        //if(postDto.getTitle() != null){
-        //    post.setTitle(postDto.getTitle());
-        //}
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
@@ -73,11 +79,12 @@ public class PostServiceImpl implements PostService {
     }
 
     //convert dto to entity
+    // convert DTO to entity
     private Post mapToEntity(PostDto postDto){
         Post post = new Post();
-        post.setTitle(post.getTitle());
-        post.setDescription(post.getDescription());
-        post.setContent(post.getContent());
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
         return post;
     }
 }
